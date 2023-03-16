3c6fe02f7650449341400d9009aa0632d768ca57
Josh Narvaez <jhnarva@gmail.com>
2023-03-15 19:30:02 -0500

    maps

--- frontend_react/src/components/Maps/Maps.jsx
+++ frontend_react/src/components/Maps/Maps.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { urlFor, client } from '../../client';
 
 import './Maps.scss';
-import { urlFor, client } from '../../client';
 
 const Maps = () => {
   const [maps, setMaps] = useState([]);
@@ -13,11 +13,11 @@ const Maps = () => {
 
     client.fetch(query)
       .then((data) => {
-        console.log(data);
         setMaps(data);
         setSelectedMap(data[0]);
-      });
-  }, [])
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   const handleMapClick = (map) => {
     setSelectedMap(map);
@@ -26,11 +26,7 @@ const Maps = () => {
   return (
     <div className="app__dashboard-map">
       {maps.map((map) => (
-        <button
-          key={map._id}
-          onClick={() => handleMapClick(map)}
-          className={selectedMap === map ? 'active' : ''}
-        >
+        <button key={map._id} onClick={() => handleMapClick(map)}>
           {map.name}
         </button>
       ))}
@@ -38,7 +34,7 @@ const Maps = () => {
         <div className="app__dashboard-map__image" style={{ backgroundImage: `url(${urlFor(selectedMap.image).url()})` }} />
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Maps;
