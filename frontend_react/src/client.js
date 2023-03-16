904c9012658e7f6dd0cfe67860c7409b1baf7c2d
Josh Narvaez <jhnarva@gmail.com>
2023-03-16 16:17:30 -0500

    images

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,15 +1,14 @@
 import { createClient } from '@sanity/client';
-import  imageUrlBuilder from '@sanity/image-url';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion: '2023-03-13',
-    useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
-    ignoreBrowserTokenWarning: true,
-  });
-  
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-03-30',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
+});
 
 const builder = imageUrlBuilder(client);
 
